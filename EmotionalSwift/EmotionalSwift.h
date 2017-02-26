080b65e9cf483d5e52a41900e81355fe2d29960a
Gerald Fairclough, Jr <gdfairclough@gmail.com>
2017-02-26 11:20:12 -0800

    Initial Commit

---
+++ EmotionalSwift/EmotionalSwift.h
@@ -0,0 +1,19 @@
+//
+//  EmotionalSwift.h
+//  EmotionalSwift
+//
+//  Created by Gerald Fairclough, Jr on 2/26/17.
+//  Copyright © 2017 Faircoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmotionalSwift.
+FOUNDATION_EXPORT double EmotionalSwiftVersionNumber;
+
+//! Project version string for EmotionalSwift.
+FOUNDATION_EXPORT const unsigned char EmotionalSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmotionalSwift/PublicHeader.h>
+
+
